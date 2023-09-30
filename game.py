c7d4a76f71627febe4115a2f27759bd120688124
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-09-30 10:58:04 -0700

    splitting into module initial

---
+++ game.py
@@ -0,0 +1,63 @@
+from blessed import Terminal
+
+term = Terminal()
+
+
+INPUT_TIMEOUT = 0.0156
+TRACK_CHARACTER = " "
+RACER_CHARACTER = "*"
+GOAL_UPPER = "=v="
+GOAL_LOWER = "=^="
+GOAL_LEFT = "|>|"  # this are going to take a multistep write
+GOAL_RIGHT = "|<|"
+STARTING_LIVES = 10
+WIN_MESSAGE = "Goal!"
+LOSE_MESSAGE = "You missed, try again or press q to quit"
+TIMEOUT_MESSAGE = "Hey, are you there?"
+
+regular = term.on_color_rgb(40, 40, 40) + term.color_rgb(255, 169, 0)
+reverse = term.color_rgb(40, 40, 40) + term.on_color_rgb(255, 169, 0)
+column_goal = term.width // 2
+row_track = term.height // 2
+
+column_track = term.width // 2
+row_goal = term.height // 2
+
+row_track = term.height // 2
+column_goal = term.width // 2
+message_row = row_track - 5
+
+
+class Game:
+    """The center of all this madness, where we setup our loop to run the game from"""
+
+    message_row = row_track - 5
+
+    def __init__(self, time, starting_lives):
+        self.runtime = time
+        self.inputkey = ""
+        self.score = 0
+        self.lives = starting_lives
+
+    def prep_screen(self):
+        """Prepare terminal for playing: clear, set goals, set track"""
+        print(term.home + term.clear)
+        print(term.move_y(row_track))
+        print(regular)
+
+    def prep_track_horizontal(self, column_goal, row_track):
+        """Prepare a horizontal track, with a goal on the lines above and below row_track"""
+        print(term.move_xy(column_goal - 1, row_track - 1) + GOAL_UPPER)
+        print(term.move_xy(column_goal - 1, row_track + 1) + GOAL_LOWER)
+        print(reverse + term.move_y(row_track) + TRACK_CHARACTER)
+        for x in range(0, term.width):
+            print(term.move_y(row_track) + term.move_x(x) + TRACK_CHARACTER)
+
+    def prep_track_vertical(self, column_track, row_goal):
+        """text goes here"""
+        # will probably need to print ANSI characters to protect the color on either side of the track
+        # just remembered that the line seems to get affected once a character is printed
+        # which will also be a thing I need to do in the diagonal. Exciting.
+        print(term.move_xy(row_goal - 1, row_goal - 1) + GOAL_LEFT)
+        print(term.move_xy(row_goal - 1, row_goal + 1) + GOAL_RIGHT)
+        print()
--- titlebar.py
+++ titlebar.py
@@ -1,8 +1,12 @@
+# Want the score on the top left, the time on the top right
 
 
 def padding_two_digits(num):
+    """Take a single digit number and turn it into a two digit number. Max 99. Negative numbers converted to absolute value"""
+    if num < 0:
+        num = abs(num)
     if num < 10:
-        return "0" + str(num)
+        return "0" + str(abs(num))
     elif num > 99:
         return "99"
     elif num >= 10 and num <= 99:
@@ -12,25 +16,55 @@ def padding_two_digits(num):
 
 
 def seconds_to_mmss(gametime):
+    """Take time in seconds and convert it into time playing in mm:ss"""
     if gametime > 3600:
-        end_game(timeout_message)
+        # end_game(timeout_message)
+        pass
     minutes = gametime % 60
     seconds = gametime - (minutes * 60)
-    return padding_two_digits(minutes)+":"+padding_two_digits(seconds)
+    return padding_two_digits(minutes) + ":" + padding_two_digits(seconds)
 
 
-"""
-def print_header(score):
-    global lives
+def print_gametime(start_time):
+    pass
+
+
+def print_lives_remaining(lives):
+    pass
+
+
+def print_header(lives, start_time, score):
+    """Print header row, including ANSI, etc."""
     remaining_lives = "Lives: " + lives
-    time = "
 
-    print(term.move_xy(0, 0) + reverse + )
-def score_ratio()
-def lives_decrement():
-    global remaining_lives -= 1
 
-def highscores_load
-def highscores_save
-def random
-"""
+# Score is going to be calculated per round, and added to a cumulative game score.
+#
+
+
+def get_current_score():
+    pass
+
+
+def score_ratio():
+    pass
+
+
+def lives_decrement(lives):
+    """Don't have a class or anything to do this with. Refactor someday."""
+    pass
+
+
+def highscores_load():
+    """Load highscores from file on start"""
+    pass
+
+
+def highscores_save():
+    """Save highscores to file on quit"""
+    pass
+
+
+def speed_tweaker():
+    """To change speed for hardest difficulty mode."""
+    pass
