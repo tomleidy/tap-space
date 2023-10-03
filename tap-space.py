bc251f10c660797f84bfc4cc117dfca74a970bf9
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-10-03 15:52:36 -0700

    chore: updated comment

--- tap-space.py
+++ tap-space.py
@@ -9,7 +9,9 @@ term = Terminal()
 # individually calling indvidual refresh buttons?
 # 
 
-# this seems to be the fastest Windows can manage it. macOS can do it much faster.
+# This is the variable that controls the speed of the game. Higher? slower game.
+# This is as fast as it can go and still be cross-platform to my current understanding.
+# Windows can only do 15.6ms timeouts. macOS can do it much faster.
 INPUT_TIMEOUT = 0.0156
 TRACK_CHARACTER = " "
 RACER_CHARACTER = "*"
