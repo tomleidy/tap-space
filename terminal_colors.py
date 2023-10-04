d31c97804a2bc3129597c1130e6e95fd6aa8bb64
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-10-03 17:35:14 -0700

    refactor: constants and screens packages

--- terminal_colors.py
+++
@@ -1,7 +0,0 @@
-"""Terminal color settings"""
-from blessed import Terminal
-
-term = Terminal()
-
-regular = term.on_color_rgb(40, 40, 40) + term.color_rgb(255, 169, 0)
-reverse = term.color_rgb(40, 40, 40) + term.on_color_rgb(255, 169, 0)
