adb6618f0dacfcc24d7fafa080047b486b6c348c
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-10-11 15:12:57 -0700

    chore: formatting

--- screen/track.py
+++ screen/track.py
@@ -7,14 +7,15 @@ from constants.terminal_strings import TRACK_CHARACTER, GOAL_UPPER, GOAL_LOWER
 # to display adjacent to the track. But that's a problem for later.
 
 col_track = term.width // 2
-row_goal = (term.height -2) // 2
-PIPE_TRACK_START_ROW = 3 # to accommodate the titlebar+1
-pipe_col_goal_left = col_track -1
+row_goal = (term.height - 2) // 2
+PIPE_TRACK_START_ROW = 3  # to accommodate the titlebar+1
+pipe_col_goal_left = col_track - 1
 pipe_col_goal_right = col_track + 1
 PIPE_GOAL_UL = f"{regular}|{term.normal}"
 PIPE_GOAL_MID_LEFT = f"{regular}>{term.normal}"
 PIPE_GOAL_MID_RIGHT = f"{regular}<{term.normal}"
 PIPE_TRACK_STRING = f"{reverse} {term.normal}"
+
 
 class Track:
     def __init__(self, shape):
@@ -25,7 +26,7 @@ class Track:
         self.print_goals()
         if self.shape == "pipe":
             self.print_track_pipe()
-        elif self.shape== "hyphen":
+        elif self.shape == "hyphen":
             self.print_track_hyphen()
 
     def get_track(self):
@@ -43,21 +44,24 @@ class Track:
             start = 0
             end = term.width
             for position in range(start, end, 1):
-                positions[position] = {"x": position, "y": row_track }
+                positions[position] = {"x": position, "y": row_track}
         elif self.shape == "pipe":
             start = PIPE_TRACK_START_ROW
             end = term.height-1
             col = term.width // 2
             for position in range(start, end, 1):
                 positions[position] = {"x": col, "y": position}
-        #print(positions)
+        # print(positions)
         self.track_positions = positions
 
     def print_track_pipe(self):
         """Print vertical track in middle of terminal"""
-        print(term.move_xy(pipe_col_goal_left, row_goal -1) + PIPE_GOAL_UL + term.move_right(1) + PIPE_GOAL_UL)
-        print(term.move_xy(pipe_col_goal_left, row_goal) + PIPE_GOAL_MID_LEFT + term.move_right(1) + PIPE_GOAL_MID_RIGHT)
-        print(term.move_xy(pipe_col_goal_left, row_goal +1) + PIPE_GOAL_UL + term.move_right(1) + PIPE_GOAL_UL)
+        print(term.move_xy(pipe_col_goal_left, row_goal - 1) +
+              PIPE_GOAL_UL + term.move_right(1) + PIPE_GOAL_UL)
+        print(term.move_xy(pipe_col_goal_left, row_goal) +
+              PIPE_GOAL_MID_LEFT + term.move_right(1) + PIPE_GOAL_MID_RIGHT)
+        print(term.move_xy(pipe_col_goal_left, row_goal + 1) +
+              PIPE_GOAL_UL + term.move_right(1) + PIPE_GOAL_UL)
         for row in range(PIPE_TRACK_START_ROW, term.height, 1):
             print(term.move_xy(col_track, row) + reverse + ' ' + term.normal)
 
@@ -71,6 +75,6 @@ class Track:
     def print_track_hyphen(self):
         """Print track in terminal"""
         if self.shape == "hyphen":
-            #print(reverse + term.move_xy(0,row_track) + TRACK_CHARACTER)
-            print(reverse + term.move_xy(0, row_track) + term.center(TRACK_CHARACTER))
-
+            # print(reverse + term.move_xy(0,row_track) + TRACK_CHARACTER)
+            print(reverse + term.move_xy(0, row_track) +
+                  term.center(TRACK_CHARACTER))
