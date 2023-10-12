3d391d094401f2d619a9f67731e315d30176f693
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-10-11 21:44:30 -0700

    chore: formatting

--- constants/game.py
+++ constants/game.py
@@ -8,7 +8,8 @@ STARTING_LIVES = 10
 INPUT_TIMEOUT = 0.0156
 
 DIFFICULTIES = [
-    {"name": "Easy" , "desc": "speed reduced, racer & goal positions shown in (x,y) format"},
+    {"name": "Easy",
+        "desc": "speed reduced, racer & goal positions shown in (x,y) format"},
     {"name": "Normal", "desc": "maximum speed, positions hidden"},
     {"name": "Hard", "desc": "speed changes in a sine wave gradient"},
     {"name": "Ultra", "desc": "speed changes randomly every 3 places"},
@@ -17,9 +18,9 @@ DIFFICULTIES = [
 
 LEVELS = [
     {"name": "Hyphen", "desc": "horizontal line"},
-    {"name": "Pipe", "desc": "vertical line" },
-    {"name": "Slash" , "desc": "diagonal line, lower left to upper right" },
-    {"name": "Backslash", "desc": "diagonal line, upper left to lower right" },
-    {"name": "Plus", "desc": "plus sign shaped" },
-    {"name": "Asterisk", "desc": "star shaped" }
+    {"name": "Pipe", "desc": "vertical line"},
+    {"name": "Slash", "desc": "diagonal line, lower left to upper right"},
+    {"name": "Backslash", "desc": "diagonal line, upper left to lower right"},
+    {"name": "Plus", "desc": "plus sign shaped"},
+    {"name": "Asterisk", "desc": "star shaped"}
 ]
